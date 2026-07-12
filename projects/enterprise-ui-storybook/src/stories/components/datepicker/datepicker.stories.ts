ef510a2d980d6cb6c9d2f30855d5d5f509abccef
Mustafizur Rohman <mustafizur.rohman88@gmail.com>
2026-07-12 08:44:23 +0200

    Update Datepicker story title to 'Components/Datepicker' in Storybook

--- projects/enterprise-ui-storybook/src/stories/components/datepicker/datepicker.stories.ts
+++ projects/enterprise-ui-storybook/src/stories/components/datepicker/datepicker.stories.ts
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from '@storybook/angular';
 import { MrDatepickerComponent } from '../../../../../enterprise-ui/src/lib/components/datepicker/mr-datepicker.component';
 
 const meta: Meta<MrDatepickerComponent> = {
-  title: 'Components/MrDatepickerComponent',
+  title: 'Components/Datepicker',
   component: MrDatepickerComponent,
 };
 
