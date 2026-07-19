6adcfdf85bbb8adaa82275b5475b8668ea9a88bb
Mustafizur Rohman <mustafizur.rohman88@gmail.com>
2026-07-19 21:39:00 +0200

    Refactor ReactiveFormWrapperComponent: add `OnDestroy` lifecycle for proper cleanup with `takeUntil`, update valueChanges subscription, and remove unused `meta` properties from Datepicker stories.

--- projects/enterprise-ui-storybook/src/stories/components/datepicker/reactive-form/reactive-form.component.ts
+++ projects/enterprise-ui-storybook/src/stories/components/datepicker/reactive-form/reactive-form.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { Subject, takeUntil } from 'rxjs';
 import { DatepickerComponent } from '../../../../../../enterprise-ui/src/lib/components/datepicker/datepicker.component';
 
 @Component({
@@ -9,10 +10,12 @@ import { DatepickerComponent } from '../../../../../../enterprise-ui/src/lib/com
   imports: [CommonModule, ReactiveFormsModule, DatepickerComponent],
   templateUrl: './reactive-form.component.html',
 })
-export class ReactiveFormWrapperComponent implements OnInit, OnChanges {
+export class ReactiveFormWrapperComponent implements OnInit, OnChanges, OnDestroy {
   @Input() label = '';
   @Input() disabled = false;
   @Input() showQuickTimeControls = false;
+
+  private destroy$ = new Subject<void>();
 
   form = new FormGroup({
     date: new FormControl({ value: null, disabled: this.disabled }),
@@ -29,8 +32,15 @@ export class ReactiveFormWrapperComponent implements OnInit, OnChanges {
   }
 
   ngOnInit(): void {
-    this.form.get('date')?.valueChanges.subscribe((value) => {
-      console.log('Datepicker value updated:', value);
-    });
+    this.form.get('date')?.valueChanges
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((value) => {
+        console.log('Datepicker value updated:', value);
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
