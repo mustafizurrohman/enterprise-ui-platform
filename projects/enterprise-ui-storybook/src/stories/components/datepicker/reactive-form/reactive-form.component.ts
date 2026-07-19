8b3f3ce5a8aeb00ea68f0a732058f8f8476d982f
Mustafizur Rohman <mustafizur.rohman88@gmail.com>
2026-07-19 21:22:08 +0200

    Refactor ReactiveFormWrapperComponent: add `OnChanges` lifecycle to handle dynamic `disabled` input, update reactive form initialization, and remove unused template binding.

--- projects/enterprise-ui-storybook/src/stories/components/datepicker/reactive-form/reactive-form.component.ts
+++ projects/enterprise-ui-storybook/src/stories/components/datepicker/reactive-form/reactive-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { DatepickerComponent } from '../../../../../../enterprise-ui/src/lib/components/datepicker/datepicker.component';
@@ -9,14 +9,24 @@ import { DatepickerComponent } from '../../../../../../enterprise-ui/src/lib/com
   imports: [CommonModule, ReactiveFormsModule, DatepickerComponent],
   templateUrl: './reactive-form.component.html',
 })
-export class ReactiveFormWrapperComponent implements OnInit {
+export class ReactiveFormWrapperComponent implements OnInit, OnChanges {
   @Input() label = '';
   @Input() disabled = false;
   @Input() showQuickTimeControls = false;
 
   form = new FormGroup({
-    date: new FormControl(null),
+    date: new FormControl({ value: null, disabled: this.disabled }),
   });
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['disabled']) {
+      if (this.disabled) {
+        this.form.get('date')?.disable();
+      } else {
+        this.form.get('date')?.enable();
+      }
+    }
+  }
 
   ngOnInit(): void {
     this.form.get('date')?.valueChanges.subscribe((value) => {
